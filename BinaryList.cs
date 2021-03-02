a4d3d67bf3151c0b6ccde838995c08572e89a65c
unknown <ana.nalbandyan@gmail.com>
2021-03-02 10:16:17 -0800

    init

---
+++ BinaryList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinaryList
+    {
+        public Node RightNode;
+        public Node LeftNode;
+    }
+
+    public void Add(Node nodeToAdd)
+    {
+
+        if (RootNode == null)
+        {
+            RootNode = nodeToAdd;
+        }
+        else
+        {
+            Node currentNode = RootNode;
+            while (true)
+            {
+                if (currentNode.NextNode == null)
+                {
+                    currentNode.NextNode = nodeToAdd;
+                    break;
+                }
+                else
+                {
+                    currentNode = currentNode.NextNode;
+                }
+            }
+        }
+    }
+
+
+    public void Search()
+    {
+
+    }
+}
+
+        
